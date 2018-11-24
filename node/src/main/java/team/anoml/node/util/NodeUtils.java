cc650743b76fbf82a65e31d40fff519e8c8844c4
deshani <deshanigeethika@gmail.com>
2018-11-24 21:34:37 +0530

    Implement createFile method in NodeUtils

--- node/src/main/java/team/anoml/node/util/NodeUtils.java
+++ node/src/main/java/team/anoml/node/util/NodeUtils.java
@@ -5,9 +5,10 @@ import team.anoml.node.core.FileTable;
 import team.anoml.node.core.RoutingTable;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Random;
-import java.util.logging.Level;
 
 public class NodeUtils {
 
@@ -23,24 +24,15 @@ public class NodeUtils {
         return DigestUtils.md5Hex(new FileInputStream(file)).toUpperCase();
     }
 
-    public static File createFile(String fileName) throws IOException {
-
+    public static void createFile(String fileName) throws IOException {
         String filePath = SystemSettings.getFilePath();
-
         Random random = new Random();
-
-        File file = File.createTempFile(fileName, ".data");
-
+        
         int length = (random.nextInt(10) + 1) * 1024 * 1024;
-
         byte[] array = new byte[length];
         random.nextBytes(array);
-        String s = new String(array, StandardCharsets.UTF_8);
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
-            bw.write(s);
-        }
-
-        return file;
+        Path file = Paths.get(filePath, fileName);
+        Files.write(file, array);
     }
 }
