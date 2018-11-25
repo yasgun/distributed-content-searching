54bc5b58a4e9bd576d20058fd88b548ce05fe02f
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2018-11-25 12:56:39 +0530

    Implement sending SER requests and trigger file search

--- node/src/main/java/team/anoml/node/util/NodeUtils.java
+++ node/src/main/java/team/anoml/node/util/NodeUtils.java
@@ -7,6 +7,9 @@ import team.anoml.node.core.RoutingTable;
 import team.anoml.node.core.RoutingTableEntry;
 
 import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -58,5 +61,21 @@ public class NodeUtils {
         Files.write(file, array, StandardOpenOption.CREATE);
 
         return file.toFile();
+    }
+
+    public static void sendRequest(DatagramSocket datagramSocket, String response, InetAddress address, int port) throws IOException {
+        String lengthText = "0000" + (response.length() + 5);
+        lengthText = lengthText.substring(lengthText.length() - 4);
+        response = lengthText + " " + response;
+
+        DatagramPacket datagramPacket = new DatagramPacket(response.getBytes(), response.length(), address, port);
+
+        for (int i = 0; i < SystemSettings.getRequestTryCount(); i++) {
+            datagramSocket.send(datagramPacket);
+            try {
+                Thread.sleep(SystemSettings.getRequestTryDelay());
+            } catch (InterruptedException ignored) {
+            }
+        }
     }
 }
