2fc498719c65a3f6bb8b471307e9b1720682b29a
deshani <deshanigeethika@gmail.com>
2018-11-25 12:02:15 +0530

    Implement TCPServer file handler

--- node/src/main/java/team/anoml/node/impl/TCPServer.java
+++ node/src/main/java/team/anoml/node/impl/TCPServer.java
@@ -1,14 +1,14 @@
 package team.anoml.node.impl;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 import team.anoml.node.api.NodeServer;
 import team.anoml.node.util.SystemSettings;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -33,28 +33,70 @@ public class TCPServer implements NodeServer {
         }
     }
 
-    private void listen() throws IOException {
+    private void listen() {
         listening = true;
 
-        serverSocket = new ServerSocket(SystemSettings.getTCPPort());
+//        serverSocket = new ServerSocket(SystemSettings.getTCPPort());
+//
+//        while (listening) {
+//            try {
+//                Socket connectionSocket = serverSocket.accept();
+//                BufferedReader in = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+//                DataOutputStream out = new DataOutputStream(connectionSocket.getOutputStream());
+//
+//                String request = in.readLine();
+//
+//                String[] incomingResult = request.split(" ", 3);
+//                String command = incomingResult[1];
+//
+//                switch (command) {
+//                    case SystemSettings.DOWN_MSG:
+//                        OutputStream fileOutputStream = new FileOutputStream("filePath");
+//                }
+//            } catch (Exception e) {
+//                logger.log(Level.WARNING, "Error occurred while TCP listening", e);
+//            }
+//        }
 
-        while (listening) {
-            try {
-                Socket connectionSocket = serverSocket.accept();
-                BufferedReader in = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-                DataOutputStream out = new DataOutputStream(connectionSocket.getOutputStream());
+        HttpServer server;
+        try {
+            server = HttpServer.create(new InetSocketAddress(SystemSettings.getTCPPort()), 0);
+            server.createContext("/", new FileHandler());
+            server.setExecutor(null); // creates a default executor
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-                String request = in.readLine();
 
-                String[] incomingResult = request.split(" ", 3);
-                String command = incomingResult[1];
+    }
 
-                switch (command) {
-                    case SystemSettings.DOWN_MSG:
-                        break;
-                }
-            } catch (Exception e) {
-                logger.log(Level.WARNING, "Error occurred while TCP listening", e);
+    static class FileHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            InputStream inputStream = t.getRequestBody();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String request = bufferedReader.readLine();
+
+            String[] incomingResult = request.split(" ", 3);
+            String command = incomingResult[1];
+
+            switch (command) {
+                case SystemSettings.DOWN_MSG:
+                    File file = new File(SystemSettings.getFilePath() + "/" + incomingResult[2]);
+                    byte[] bytes = new byte[(int) file.length()];
+                    FileInputStream fileInputStream = new FileInputStream(file);
+
+                    t.sendResponseHeaders(200, file.length());
+                    OutputStream os = t.getResponseBody();
+
+                    int count;
+                    while ((count = fileInputStream.read(bytes)) > 0) {
+                        os.write(bytes, 0, count);
+                    }
+                    os.close();
+                    fileInputStream.close();
             }
         }
     }
