cf6727f4a70bff53008be8675391fd27dfcad787
yasgun <yasas.14@cse.mrt.ac.lk>
2018-11-27 21:25:10 +0530

    Added response tracker entry at join request sending

--- node/src/main/java/team/anoml/node/sender/request/JoinRequestSender.java
+++ node/src/main/java/team/anoml/node/sender/request/JoinRequestSender.java
@@ -2,9 +2,11 @@ package team.anoml.node.sender.request;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import team.anoml.node.core.ResponseTracker;
 import team.anoml.node.util.SystemSettings;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class JoinRequestSender extends AbstractRequestSender {
 
@@ -13,6 +15,7 @@ public class JoinRequestSender extends AbstractRequestSender {
     protected void sendRequest() {
         try {
             String request = String.format(SystemSettings.JOIN_MSG_FORMAT, nodeIpAddress, nodePort);
+            ResponseTracker.getResponseTracker().addWaitingResponse(SystemSettings.JOINOK_MSG, getDestinationIpAddress(), getDestinationPort(), new Date());
             sendMessage(request, getDestinationIpAddress(), getDestinationPort());
             logger.info("Sent JOIN request to " + getDestinationIpAddress() + ":" + getDestinationPort());
 
